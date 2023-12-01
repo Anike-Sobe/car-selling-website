d42c126fe0dae0d5a9574967c49908e9089ae2b7
Anike-Sobe <anikevictor469@gmail.com>
2023-11-30 21:13:11 -0800

    I finished the javascript for the home section

--- script.js
+++ script.js
@@ -47,7 +47,17 @@ document.querySelector('.home').onmousemove = (e) => {
         let x = (window.innerWidth - e.pageX * speed) / 90;
         let y = (window.innerHeight - e.pageY * speed) / 90;
 
-        elm.style.transform = 'translateX(${y}px) translateX(${x}px)';
+        elm.style.transform = `translateX(${y}px) translateX(${x}px)`
     });
 
-}
+};
+
+
+document.querySelector('.home').onmouseleave = () => {
+
+    document.querySelectorAll('.home-parallax').forEach (elm =>{
+
+        elm.style.transform = `translateX(0px) translateX(0px)`;
+    });
+
+};
